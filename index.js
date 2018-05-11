d80373308c682c151805a1b63d91a66da3b275bd
Shama Hoque <shama.hoque@gmail.com>
2018-05-11 16:25:28 -0700

    Load sample game data to state and change background with Environment

--- index.js
+++ index.js
@@ -4,9 +4,54 @@ import {
   StyleSheet,
   Text,
   View,
+  Environment
 } from 'react-360';
 
 export default class MERNVR extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      game: {
+        name: 'Space Exploration',
+        world: 'https://s3.amazonaws.com/mernbook/vrGame/milkyway.jpg',
+        answerObjects: [
+          {
+            objUrl: 'https://s3.amazonaws.com/mernbook/vrGame/planet.obj',
+            mtlUrl: 'https://s3.amazonaws.com/mernbook/vrGame/planet.mtl',
+            translateX: -50,
+            translateY: 0,
+            translateZ: 30,
+            rotateX: 0,
+            rotateY: 0,
+            rotateZ: 0,
+            scale: 7,
+            color: 'white'
+          }
+        ],
+        wrongObjects: [
+          {
+            objUrl: 'https://s3.amazonaws.com/mernbook/vrGame/tardis.obj',
+            mtlUrl: 'https://s3.amazonaws.com/mernbook/vrGame/tardis.mtl',
+            translateX: 0,
+            translateY: 0,
+            translateZ: 90,
+            rotateX: 0,
+            rotateY: 20,
+            rotateZ: 0,
+            scale: 1,
+            color: 'white'
+          }
+        ]
+      }
+    }
+  }
+  componentDidMount = () => {
+    let vrObjects = this.state.game.answerObjects.concat(this.state.game.wrongObjects)
+    this.setState({vrObjects: vrObjects})
+    Environment.setBackgroundImage(
+      {uri: this.state.game.world}
+    )
+  }
   render() {
     return (
       <View style={styles.panel}>
